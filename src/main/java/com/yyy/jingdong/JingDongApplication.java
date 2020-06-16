d525644fee49476520deec98138a2af015512362
yyy <xxyangyuyao@163.com>
2020-06-16 10:27:15 +0800

    京东爬虫

---
+++ src/main/java/com/yyy/jingdong/JingDongApplication.java
@@ -0,0 +1,13 @@
+package com.yyy.jingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingDongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingDongApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yyy/jingdong/process/JingDongProcess.java
@@ -0,0 +1,14 @@
+package com.yyy.jingdong.process;
+
+/**
+ * @ClassName JingDongProcess
+ * @Description TODO
+ * @Author yuyao.yang
+ * @Date 2020/6/16 9:38
+ */
+public class JingDongProcess {
+
+
+
+
+}
